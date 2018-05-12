7c5269ca09052bc1df79bf1aafbdf00b2dfde5d5
dylanptcu <dylan.perez@tcu.edu>
2018-05-12 10:22:19 -0500

    file constants

--- PHPMySQLBook/index.php
+++ PHPMySQLBook/index.php
@@ -7,13 +7,19 @@
 <body>
 <?php echo "Today is " . date("1") . ".\n"; ?>
 <?php echo "Hello World\n" ?>
-<br>
+<br><hr>
 Example 3-4:<br>
 <?php
     $username = "Dylan Perez";
     echo $username . "<br>";
     $current_user = $username;
     echo $current_user;
+?>
+
+<br><hr>
+File constants __line__ and __dir__:<br>
+<?php
+    echo "This is line " . __LINE__ . " of file " . __FILE__ . ".<br>";
 ?>
 </body>
 </html>
