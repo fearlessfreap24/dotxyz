e1c9b7e05a4680e6ff276ab3221470f6bfead552
dylanptcu <dylan.perez@tcu.edu>
2018-05-12 10:06:28 -0500

    example 3-4

--- PHPMySQLBook/index.php
+++ PHPMySQLBook/index.php
@@ -6,5 +6,14 @@
 </head>
 <body>
 <?php echo "Today is " . date("1") . ".\n"; ?>
+<?php echo "Hello World\n" ?>
+<br>
+Example 3-4:<br>
+<?php
+    $username = "Dylan Perez";
+    echo $username . "<br>";
+    $current_user = $username;
+    echo $current_user;
+?>
 </body>
 </html>
